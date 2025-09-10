f0506b5f48c7788e4a733dd4b840dff3d489b89b
Dex <dibyanshu.singh2004@gmail.com>
2025-09-10 03:52:42 +0000

    make the always in sync section slidable for mobile view and hide the sl

--- src/components/sync-status-cards.tsx
+++ src/components/sync-status-cards.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { Card, CardContent } from '@/components/ui/card';
 import { Clock, InfinityIcon, Music, Waves } from 'lucide-react';
 import { format } from 'date-fns';
 import { toZonedTime } from 'date-fns-tz';
